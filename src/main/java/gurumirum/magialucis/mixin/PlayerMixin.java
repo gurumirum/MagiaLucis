cff29eca8655bf86866cbb17b341d0132f3027d7
Tictim <ksw07149@gmail.com>
2025-01-24 22:26:02 +0900

    Wreaths give apples golden apple effect

--- src/main/java/gurumirum/magialucis/mixin/PlayerMixin.java
+++ src/main/java/gurumirum/magialucis/mixin/PlayerMixin.java
@@ -1,17 +1,27 @@
 package gurumirum.magialucis.mixin;
 
+import gurumirum.magialucis.contents.Accessories;
+import gurumirum.magialucis.contents.ModCurioSlots;
 import gurumirum.magialucis.contents.ModDataComponents;
 import gurumirum.magialucis.contents.Wands;
 import gurumirum.magialucis.contents.item.wand.LapisShieldItem;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
+import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity {
@@ -27,6 +37,40 @@ public abstract class PlayerMixin extends LivingEntity {
 			long charge = stack.getOrDefault(ModDataComponents.LUX_CHARGE, 0L);
 			stack.set(ModDataComponents.LUX_CHARGE, Math.max(0, charge - LapisShieldItem.COST_PER_SHIELD_DISABLE));
 			if (charge >= LapisShieldItem.COST_PER_SHIELD_DISABLE) info.cancel();
+		}
+	}
+
+	private boolean magialucis$eatReentrantFlag;
+
+	@SuppressWarnings("DataFlowIssue")
+	@Inject(method = "eat", at = @At("HEAD"), cancellable = true)
+	public void magialucis$eat(Level level, ItemStack food, FoodProperties foodProperties,
+	                           CallbackInfoReturnable<ItemStack> info) {
+		if (this.magialucis$eatReentrantFlag) return;
+
+		Player player = (Player)(Object)this;
+
+		if (food.is(Items.APPLE)) {
+			ICuriosItemHandler h = CuriosApi.getCuriosInventory(player).orElse(null);
+			if (h != null) {
+				ICurioStacksHandler curios = h.getCurios().get(ModCurioSlots.HEADWEAR);
+				if (curios != null) {
+					IDynamicStackHandler stacks = curios.getStacks();
+					for (int i = 0; i < stacks.getSlots(); ++i) {
+						ItemStack stack = stacks.getStackInSlot(i);
+						if (stack.is(Accessories.DRUID_WREATH.asItem()) || stack.is(Accessories.DRYAD_WREATH.asItem())) {
+							this.magialucis$eatReentrantFlag = true;
+							try {
+								ItemStack result = this.eat(level, food, Foods.GOLDEN_APPLE);
+								info.setReturnValue(result);
+							} finally {
+								this.magialucis$eatReentrantFlag = false;
+							}
+							return;
+						}
+					}
+				}
+			}
 		}
 	}
 }
